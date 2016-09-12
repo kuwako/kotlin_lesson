c0a2581c43daa63866b7414c00a43d14b6194cf8
kuwako <m.kuwako0702@gmail.com>
2016-09-12 11:00:37 +0000

    KotlinLesson8

--- prac_6.kt
+++ prac_6.kt
@@ -1,3 +1,9 @@
 fun main(args: Array<String>) {
-  
+  val prices = listOf(53.2, 48.2, 32.8)
+  prices
+  /*.map { n -> n * 1.08 } // 引数 -> 処理*/
+  .map { it * 1.08 }
+  /*.filter { n -> n > 50 } */
+  .filter { it > 50 }
+  .forEach { println(it) }
 }
