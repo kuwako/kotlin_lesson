2f219526b07fc4477c60b6ae33cbf98636586ac0
kuwako <m.kuwako0702@gmail.com>
2016-09-12 10:47:39 +0000

    KotlinLesson7

--- prac_5.kt
+++ prac_5.kt
@@ -1,7 +1,6 @@
 // Collection
 /*
   - List
-  - Set
   - Map
 */
 
@@ -16,8 +15,14 @@ fun main(args: Array<String>) {
   val salesMutable = mutableListOf(20, 30, 40)
   println(salesMutable[1])
   salesMutable[1] = 50
-  for (saleM in salesMutable) {
-    println(saleM)
+  for (saleMut in salesMutable) {
+    println(saleMut)
   }
 
+  val users = mapOf("kuwako" to 10, "masaki" to 20, "mask" to 30)
+  println(users["kuwako"])
+  println(users.size)
+  println(users.keys)
+  println(users.values)
+  println(users.entries)
 }
